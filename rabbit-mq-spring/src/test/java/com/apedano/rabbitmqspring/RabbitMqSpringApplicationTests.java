273a751541ba22b3ce0a832d41559b1a84bd7e1f
Alessandro Pedano <pedano.alessandro@gmail.com>
2021-10-04 08:49:09 +0200

    [rabbit-mp-spring] added initialized application

---
+++ rabbit-mq-spring/src/test/java/com/apedano/rabbitmqspring/RabbitMqSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apedano.rabbitmqspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitMqSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
